6a157a757a00ef16eb902cab0f187280df2058f3
Ali <ali-a-ahmed1097@hotmail.com>
2022-12-07 15:59:34 -0500

    Finished a lot. Only core implementation left is the edit button.

--- library.js
+++ library.js
@@ -1,3 +1,7 @@
+let books = [];
+const cardButtonText = ['Toggle Read', 'Edit', 'Remove'];
+const cardDivText = ['Title: ', 'Author: ', 'Pages: '];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -12,6 +16,8 @@ Book.prototype.info = function () {
 const newBook = document.querySelector('.new');
 const overlay = document.getElementById('overlay');
 const form = document.getElementById('form');
+const inputs = form.querySelectorAll('input');
+const addBookBtn = document.getElementById('add');
 
 newBook.addEventListener('click', () => {
     overlay.classList.add('overlay');
@@ -23,13 +29,68 @@ function removeOverlay() {
     overlay.classList.remove('overlay');
     form.classList.remove('form');
     form.classList.toggle('hide');
-    const inputs = form.querySelectorAll('input');
+
     inputs.forEach(input => {
         input.value = '';
         input.checked = false;
     });
 }
 
+function addBook() {
+    if (inputs[0].value !== '' && inputs[1].value !== '' && inputs[2].value !== '') {
+        books.push(new Book(inputs[0].value, inputs[1].value, inputs[2].value - 0, inputs[3].checked));
+
+        const booksDiv = document.getElementById('books');
+        const newCard = document.createElement('div');
+        delete newBook;
+
+        for (let i = 0; i < 3; i++) {
+            newCard.appendChild(document.createElement('div'));
+            newCard.querySelector('div:last-of-type').textContent = cardDivText[i] + books[books.length - 1][inputs[i].name];
+        }
+
+        newCard.appendChild(document.createElement('div'));
+        const status = books[books.length - 1].read;
+        const statusDiv = newCard.querySelector('div:last-of-type');
+        if (status)
+            statusDiv.textContent = `Status: Read`;
+        else
+            statusDiv.textContent = 'Status: Not Read';
+
+        for (let i = 0; i < 3; i++) {
+            newCard.appendChild(document.createElement('button'));
+            newCard.querySelector('button:last-of-type').textContent = cardButtonText[i];
+        }
+
+        newCard.querySelector('button:nth-of-type(1)').addEventListener('click', function () {
+            books[books.length - 1].read = !books[books.length - 1].read;
+            const statusDiv = newCard.querySelector('div:last-of-type');
+            if (books[books.length - 1].read)
+                statusDiv.textContent = `Status: Read`;
+            else
+                statusDiv.textContent = 'Status: Not Read';
+        });
+
+        newCard.querySelector('button:nth-of-type(2)').addEventListener('click', function () {
+            
+        });
+
+        newCard.querySelector('button:nth-of-type(3)').addEventListener('click', function () {
+            delete books[books.length - 1];
+            newCard.remove();
+        });
+
+        newCard.classList.add('card');
+        booksDiv.appendChild(newCard);
+        booksDiv.appendChild(newBook);
+
+        removeOverlay();
+    } else {
+        console.log('here');
+    }
+}
+
 const cancel = document.getElementById('cancel');
 cancel.addEventListener('click', removeOverlay);
 overlay.addEventListener('click', removeOverlay);
+addBookBtn.addEventListener('click', addBook)
