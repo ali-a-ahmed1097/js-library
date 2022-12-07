14c143dbc16dbd98a67bc65d8a100d45a92cd90d
Ali <ali-a-ahmed1097@hotmail.com>
2022-12-07 13:06:48 -0500

    Finished styling the overlay form.

--- library.js
+++ library.js
@@ -3,8 +3,28 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? 'read' : 'not read yet'}`
-    }
 }
+
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read ? 'read' : 'not read yet'}`
+}
+
+const btn = document.getElementById('new');
+const overlay = document.getElementById('overlay');
+const form = document.getElementById('form');
+
+btn.addEventListener('click', () => {
+    overlay.classList.add('overlay');
+    form.classList.add('form');
+    form.classList.toggle('hide');
+});
+
+function removeOverlay() {
+    overlay.classList.remove('overlay');
+    form.classList.remove('form');
+    form.classList.toggle('hide');
+}
+
+const cancel = document.getElementById('cancel');
+cancel.addEventListener('click', removeOverlay);
+overlay.addEventListener('click', removeOverlay);
